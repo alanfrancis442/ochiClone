f02e1e7e61fdaae87de905f0f7a6927926ed390b
alanfrancis442 <alan.prime567@gmail.com>
2024-02-14 23:13:01 +0530

    loco added sucessfully

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import Scroller from "./scroller";
 import Hero1 from "./hero1";
 import Eyes from "./eyes";
 import Projects from "./projects";
+import LocomotiveScroll from 'locomotive-scroll';
+
 export default function Home() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
     <Nav />
